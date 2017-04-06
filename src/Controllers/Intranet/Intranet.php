e6d4e01106d3c1e0239d6dc39eac249080764472
Jose Luis Rosado Cardenas <joser@enzacta.net>
2017-04-06 05:45:05 -0500

    test render

--- src/Controllers/Intranet/Intranet.php
+++ src/Controllers/Intranet/Intranet.php
@@ -11,12 +11,12 @@ class Intranet
         $app = Slim::getInstance();
         $msgToEncrypt = $app->request()->post('strToEncriptar');
         $msgToDesencrypt = $app->request()->post('strToDesencrypt');
-        $app->response->setBody("    <form method=\"post\">
-        <label>encriptar:</label><input name=\"strToEncriptar\"> ". Security::encrypt($msgToEncrypt). "
-         <br>
-        <label>Desencriptar:</label><input name=\"strToDesencrypt\">".Security::desencrypt($msgToDesencrypt)."<br>
-        <input type=\"submit\">
-    </form>");
-//        $app->render('Intranet\index.php',array('msgToEncrypt'=>$msgToEncrypt, "msgToDesencrypt"=>$msgToDesencrypt) );
+//        $app->response->setBody("    <form method=\"post\">
+//        <label>encriptar:</label><input name=\"strToEncriptar\"> ". Security::encrypt($msgToEncrypt). "
+//         <br>
+//        <label>Desencriptar:</label><input name=\"strToDesencrypt\">".Security::desencrypt($msgToDesencrypt)."<br>
+//        <input type=\"submit\">
+//    </form>");
+        $app->render('Intranet\index.php',array('msgToEncrypt'=>$msgToEncrypt, "msgToDesencrypt"=>$msgToDesencrypt) );
     }
 }
